b04ead83672a03be196dee25bb1eb10f3fea77fc
scoiatael <czapl.luk@gmail.com>
2014-03-03 16:55:38 +0100

    uglify, jshint, react working

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,4 @@
+/*jshint node:true*/
 'use strict';
 
 module.exports = function(grunt) {
@@ -10,16 +11,43 @@ module.exports = function(grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'src/<%= pkg.name %>.js',
+        src: ['src/**/<%= pkg.name %>.js', 'react/**/<%= pkg.name %>.js'],
         dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        jshintrc: 'jshintrc.json'    
+      },
+      common:      ['gruntfile.js', 'lib/**/*.js', 'test/**/*.js'],
+      react:       ['react/**/*.js'],
+      beforebuild: ['src/**/*.js'],
+      afterbuild:  ['build/**/*.js']
+    },
+    react: {
+      files: {
+        expand: true,
+        cwd: 'src/',
+        src: ['**/*.jsx'],
+        dest: 'react/',
+        ext: '.js'
+      }
+    },
+    watch: {
+      jsx: {
+        files: ['src/**/*.jsx'],
+        tasks: ['react', 'jshint:react']
       }
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
+  // Load plugins that provide tasks.
+  grunt.loadNpmTasks('grunt-react');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['jshint:common', 'jshint:beforebuild', 'react', 'jshint:react', 'uglify']);
 
 };
